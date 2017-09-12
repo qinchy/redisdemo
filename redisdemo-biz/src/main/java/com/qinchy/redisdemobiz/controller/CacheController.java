2cb4002471e17ae666146d02e9282a59d062117e
qinchy <qinchy@msn.com>
2017-09-12 17:53:22 +0800

    first commit

--- redisdemo-biz/src/main/java/com/qinchy/redisdemobiz/controller/CacheController.java
+++ redisdemo-biz/src/main/java/com/qinchy/redisdemobiz/controller/CacheController.java
@@ -1,6 +1,7 @@
 package com.qinchy.redisdemobiz.controller;
 
 import com.qinchy.redisdemodao.config.RedisUtils;
+import com.qinchy.redisdemodao.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,25 @@ public class CacheController {
     @Autowired
     private RedisUtils redisUtils;
 
-    @RequestMapping("/test")
-    public String test(@RequestParam(value = "key") String key, @RequestParam(value = "value") String value) {
+    @RequestMapping("/setstring")
+    public String setString(@RequestParam(value = "key") String key, @RequestParam(value = "value") String value) {
+        log.info("进入了setString方法");
         redisUtils.set(key, value);
-        log.info("进入了方法");
         String string = redisUtils.get(key).toString();
         return string;
+    }
+
+    @RequestMapping("/setobject")
+    public String setObject(@RequestParam(value = "username") String username, @RequestParam(value = "age") Integer age) {
+        log.info("进入了setObject方法");
+        User user = new User(username, age);
+        boolean setFlag = redisUtils.set(username, user);
+        log.info("set方法返回值：" + setFlag);
+        Object o = redisUtils.get(username);
+        if (null != o) {
+            return o.toString();
+        }
+        return null;
     }
 
 }
--- redisdemo-dao/src/main/java/com/qinchy/redisdemodao/config/RedisConfig.java
+++ redisdemo-dao/src/main/java/com/qinchy/redisdemodao/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.qinchy.redisdemodao.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qinchy.redisdemodao.domain.User;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
@@ -14,6 +15,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.lang.reflect.Method;
 
@@ -62,16 +64,30 @@ public class RedisConfig extends CachingConfigurerSupport {
      * @param factory
      * @return
      */
+//    @Bean
+//    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+//        StringRedisTemplate template = new StringRedisTemplate(factory);
+//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+//        ObjectMapper om = new ObjectMapper();
+//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+//        jackson2JsonRedisSerializer.setObjectMapper(om);
+//        template.setValueSerializer(jackson2JsonRedisSerializer);   //如果key是String 需要配置一下StringSerializer,不然key会乱码 /XX/XX
+//        template.afterPropertiesSet();
+//        return template;
+//    }
+
+    /**
+     * RedisTemplate配置
+     * @param factory
+     * @return
+     */
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
-        StringRedisTemplate template = new StringRedisTemplate(factory);
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-        ObjectMapper om = new ObjectMapper();
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(om);
-        template.setValueSerializer(jackson2JsonRedisSerializer);   //如果key是String 需要配置一下StringSerializer,不然key会乱码 /XX/XX
-        template.afterPropertiesSet();
+    public RedisTemplate<String, User> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, User> template = new RedisTemplate<String, User>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new RedisObjectSerializer());
         return template;
     }
 
